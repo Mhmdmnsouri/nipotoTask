fb1d350ad4949ee1c9976092249431993737187a
Mhmdmnsouri <Bitshawcoder@gmail.com>
2024-11-10 20:53:27 +0330

    minor style modify

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex justify-center items-center mb-20 text-2xl font-medium w-3/4 sm:w-1/2">
         <div class="relative w-full">
-            <input type="text" v-model="query" placeholder="Search..." class="border p-2 rounded w-full pr-10" />
+            <input type="text" v-model="query" placeholder="Search..." class="border p-2 rounded w-full pr-12" />
             <button @click="onSearch"
                 class="absolute right-0 top-0 m-[5px] bg-red-500 p-2 rounded hover:bg-red-600 transition-colors">
                 <Icon icon="mdi:search" class="text-white" />

d2360bb099b5ffeef7cfbca9454d5f8751333330
Mhmdmnsouri <Bitshawcoder@gmail.com>
2024-11-10 20:53:14 +0330

    add fav system

--- src/components/CardBox.vue
+++ src/components/CardBox.vue
@@ -2,7 +2,8 @@
     <div class="flex flex-col bg-[#f3f3f3] w-[250px] hover:translate-y-[-5px] transition-transform">
         <div class="relative">
             <img :src="imgSrc" alt="product image">
-            <Icon class="absolute top-3 left-3 text-[25px]" color="red" icon="mdi-light:heart"></Icon>
+            <Icon class="absolute top-3 left-3 text-[25px] cursor-pointer" :color="isFavorite ? 'red' : 'gray'"
+                :icon="isFavorite ? 'mdi-heart' : 'mdi-light:heart'" @click="toggleFavorite"></Icon>
         </div>
         <div class="p-2">
             <h2 class="font-medium text-xl">{{ title }}</h2>
@@ -13,21 +14,51 @@
 
 <script setup>
 import { Icon } from '@iconify/vue/dist/iconify.js';
-import { defineProps } from 'vue';
+import { defineProps, ref, onMounted, watch } from 'vue';
 
 const props = defineProps({
-    title: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    imgSrc: {
-        type: String,
-        required: true,
+    title: String,
+    description: String,
+    imgSrc: String,
+    id: Number
+});
+
+const isFavorite = ref(false);
+
+onMounted(() => {
+    const favoriteItems = JSON.parse(localStorage.getItem('favoriteItems')) || [];
+    isFavorite.value = favoriteItems.includes(props.id);
+});
+
+const toggleFavorite = () => {
+    const favoriteItems = JSON.parse(localStorage.getItem('favoriteItems')) || [];
+
+    if (isFavorite.value) {
+        const index = favoriteItems.indexOf(props.id);
+        if (index > -1) {
+            favoriteItems.splice(index, 1);
+        }
+    } else {
+        favoriteItems.push(props.id);
     }
+
+    isFavorite.value = !isFavorite.value;
+    localStorage.setItem('favoriteItems', JSON.stringify(favoriteItems));
+};
+
+watch(isFavorite, (newValue) => {
+    const favoriteItems = JSON.parse(localStorage.getItem('favoriteItems')) || [];
+    if (newValue) {
+        if (!favoriteItems.includes(props.id)) {
+            favoriteItems.push(props.id);
+        }
+    } else {
+        const index = favoriteItems.indexOf(props.id);
+        if (index > -1) {
+            favoriteItems.splice(index, 1);
+        }
+    }
+    localStorage.setItem('favoriteItems', JSON.stringify(favoriteItems));
 });
 </script>
 
